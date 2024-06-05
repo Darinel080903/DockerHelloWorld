aa179de1d6b197bf41ae6446ce0b76d6fe05f832
Darinel Monjaras Ruiz <darinelmr12345@gmail.com>
2024-06-05 15:07:52 -0600

    last changes

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 const port = 3000;
 
 app.get('/api/v1/welcome', (req, res) => {
-  res.send('Hello, World! x2 HELOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOO');
+  res.send('Hello, World! x2 HELOOOOOOOOOOOOOOOOO');
 });
 
 app.listen(port, () => {
